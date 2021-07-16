d3d7506a65c8f60e9b656e97d691fec21264bff9
long2ice <long2ice@gmail.com>
2021-07-16 16:20:35 +0800

    update

--- tests/test_xiaoai.py
+++ tests/test_xiaoai.py
@@ -1,10 +1,10 @@
 import os
+
 import pytest
+
 from xiaoai import XiaoAi
 
-xiaomi = XiaoAi(
-    user=os.getenv("XIAOAI_USER"), password=os.getenv("XIAOAI_PASSWORD")
-)
+xiaomi = XiaoAi(user=os.getenv("XIAOAI_USER"), password=os.getenv("XIAOAI_PASSWORD"))
 
 
 @pytest.mark.asyncio
@@ -15,6 +15,14 @@ async def test_get_login_sign():
 
 
 @pytest.mark.asyncio
+@pytest.fixture
 async def test_service_auth(test_get_login_sign):
     ret = await xiaomi._service_auth(test_get_login_sign)
-    print(ret)
+    assert "qs" in ret
+    assert "ssecurity" in ret
+    return ret
+
+
+@pytest.mark.asyncio
+async def test_login_miai(test_service_auth):
+    await xiaomi.login_miai(test_service_auth)
--- xiaoai/__init__.py
+++ xiaoai/__init__.py
@@ -1,8 +1,10 @@
 import base64
 import json
 from hashlib import md5, sha1
+from typing import Any
 
 import httpx
+
 from xiaoai import constants
 from xiaoai.exceptions import ServiceAuthError
 
@@ -13,47 +15,50 @@ class XiaoAi:
     def __init__(self, user: str, password: str):
         self._user = user
         self._password = password
+        self.client = httpx.AsyncClient()
 
     def _parse_response(self, content: str):
-        return json.loads(content.replace('&&&START&&&', ''))
+        return json.loads(content.replace("&&&START&&&", ""))
 
     def _get_client_sign(self, nonce: str, ssecurity: str):
-        s = f'nonce={nonce}&{ssecurity}'
-        return base64.b64encode(sha1(s.encode()).digest())
+        s = f"nonce={nonce}&{ssecurity}"
+        return base64.b64encode(sha1(s.encode()).digest()).decode()
 
-    async def login_miai(self, auth_info: dict):
-        nonce = auth_info.get('nonce')
-        ssecurity = auth_info.get('ssecurity')
-        location = auth_info.get('location')
+    async def login_miai(self, auth_info: dict[str, Any]):
+        nonce = auth_info.get("nonce", "")
+        ssecurity = auth_info.get("ssecurity", "")
+        location = auth_info.get("location", "")
         sign = self._get_client_sign(nonce, ssecurity)
-        async with httpx.AsyncClient() as client:
-            res = await client.get(location, params={
-                'clientSign': sign
-            })
+        res = await self.client.get(location, params={"clientSign": sign})
+        return res.json()
 
     async def _get_login_sign(self):
-        async with httpx.AsyncClient() as client:
-            res = await client.get(
-                constants.SERVICE_LOGIN_URL, params=self._common_params
-            )
-            ret = self._parse_response(res.text)
-            return {'_sign': ret.get('_sign'), 'qs': ret.get('qs')}
+        res = await self.client.get(constants.SERVICE_LOGIN_URL, params=self._common_params)
+        ret = self._parse_response(res.text)
+        return {"_sign": ret.get("_sign"), "qs": ret.get("qs")}
 
     async def _service_auth(self, sign: dict):
-        async with httpx.AsyncClient() as client:
-            res = await client.post(
-                constants.SERVICE_AUTH_URL, data={
-                    'user': self._user,
-                    'hash': md5(self._password.encode()).hexdigest().upper(),
-                    'callback': 'https://api.mina.mi.com/sts',
-                    **self._common_params,
-                    **sign
-                },
-                headers={
-                    'Cookie': f'deviceId={constants.APP_DEVICE_ID};sdkVersion={constants.SDK_VER}'
-                }
-            )
-            ret = self._parse_response(res.text)
-            if ret.get('code') != 0:
-                raise ServiceAuthError(ret.get('desc'))
-            return ret
+        res = await self.client.post(
+            constants.SERVICE_AUTH_URL,
+            data={
+                "user": self._user,
+                "hash": md5(self._password.encode()).hexdigest().upper(),
+                "callback": "https://api.mina.mi.com/sts",
+                **self._common_params,
+                **sign,
+            },
+            headers={
+                "Cookie": f"deviceId={constants.APP_DEVICE_ID};sdkVersion={constants.SDK_VER}"
+            },
+        )
+        ret = self._parse_response(res.text)
+        if ret.get("code") != 0:
+            raise ServiceAuthError(ret.get("desc"))
+        return ret
+
+    async def get_devices(self):
+        pass
+
+    async def tts(self, text: str):
+        url = constants.USBS_URL
+        params = {"deviceId": ""}
